f8a3e8bce8a9c4322471e2edcdb22eb28eb5f0db
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-11-26 22:34:47 +0600

    connect server client

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
+        //add
         await client.connect();
 
         const db = client.db('parcelDB'); // database name
