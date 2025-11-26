c18a4e921fe2f7d4a18b3f6476f609e702c583eb
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-11-26 22:33:52 +0600

    add mongoclient

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k8hbq0j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+//add
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
